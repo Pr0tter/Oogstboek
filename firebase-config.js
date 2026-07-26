a3ef5c4707e53bea4b317bb2087de7d50241c620
Pr0tter <Wlaceur@gmail.com>
2026-07-26 21:51:46 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase-configuratie voor Oogstboek.
+// Dit is HETZELFDE Firebase-project als je FitLog/Macros-app (fitlog-app-fb469).
+//
+// Dit bestand wordt apart geladen zodat het NOOIT overschreven wordt
+// als je later een nieuwe index.html van mij uploadt.
+
+window.OOGSTBOEK_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD41Le7bUXHD3oUSHN1fyldRKhEs9OzM48",
+  authDomain: "fitlog-app-fb469.firebaseapp.com",
+  projectId: "fitlog-app-fb469",
+  storageBucket: "fitlog-app-fb469.firebasestorage.app",
+  messagingSenderId: "862661468874",
+  appId: "1:862661468874:web:479086ec36f59f7e328a33"
+};
